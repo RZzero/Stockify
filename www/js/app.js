e627ff4db0bcf12c0a3b4bac147a1da1a7004b8d
Jose Clovis <c.ramirez@gbh.com.do>
2018-01-01 09:48:07 -0400

    Adds project structure

--- www/js/app.js
+++ www/js/app.js
@@ -25,61 +25,50 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
 
 .config(function($stateProvider, $urlRouterProvider) {
 
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
+  // Set the application's routing.
   $stateProvider
-
-  // setup an abstract state for the tabs directive
     .state('tab', {
     url: '/tab',
     abstract: true,
     templateUrl: 'templates/tabs.html'
   })
-
-  // Each tab has its own nav history stack:
-
-  .state('tab.dash', {
-    url: '/dash',
+  .state('tab.overview', {
+    url: '/overview',
     views: {
-      'tab-dash': {
-        templateUrl: 'templates/tab-dash.html',
-        controller: 'DashCtrl'
+      'tab-overview': {
+        templateUrl: 'templates/tab-overview.html',
       }
     }
   })
-
-  .state('tab.chats', {
-      url: '/chats',
+  .state('tab.history', {
+      url: '/history',
       views: {
-        'tab-chats': {
-          templateUrl: 'templates/tab-chats.html',
-          controller: 'ChatsCtrl'
+        'tab-history': {
+          templateUrl: 'templates/tab-history.html',
         }
       }
     })
-    .state('tab.chat-detail', {
-      url: '/chats/:chatId',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/chat-detail.html',
-          controller: 'ChatDetailCtrl'
+    .state('tab.favorite', {
+        url: '/favorite',
+        views: {
+          'tab-favorite': {
+            templateUrl: 'templates/tab-favorite.html',
+          }
         }
-      }
-    })
+      })
+      .state('tab.win-lose', {
+          url: '/win-lose',
+          views: {
+            'tab-win-lose': {
+              templateUrl: 'templates/tab-win-lose.html',
+            }
+          }
+        })
 
-  .state('tab.account', {
-    url: '/account',
-    views: {
-      'tab-account': {
-        templateUrl: 'templates/tab-account.html',
-        controller: 'AccountCtrl'
-      }
-    }
-  });
+
+  ;
 
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/dash');
+  $urlRouterProvider.otherwise('/tab/overview');
 
 });
